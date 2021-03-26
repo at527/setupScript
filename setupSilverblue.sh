7648f921c8c2cd8c2724f4377ab769e978e98785
at527 <59618726+at527@users.noreply.github.com>
2021-03-25 23:54:29 -0700

    comment ostree struff, add firefox flathub

--- setupSilverblue.sh
+++ setupSilverblue.sh
@@ -8,24 +8,24 @@ echo "Installing Gnome Tweaks"
 # Add gnome-tweaks
 rpm-ostree install gnome-tweaks -y
 
-echo "Enabling RPMFusion"
+# echo "Enabling RPMFusion"
 # Add RPMFusion
-rpm-ostree install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# rpm-ostree install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-echo "Installing Media Codecs"
+# echo "Installing Media Codecs"
 # Add codecs
-rpm-ostree groupupdate core -y
-rpm-ostree groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-rpm-ostree groupupdate -y sound-and-video
+# rpm-ostree groupupdate core -y
+# rpm-ostree groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+# rpm-ostree groupupdate -y sound-and-video
 
 echo "Adding Flathub"
 # Add Flathub
 flatpak remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo "Installing Zoom, Slack, Discord"
+echo "Installing Zoom, Slack, Discord, Firefox"
 # Install Apps
 # Zoom, Slack, Discord
-flatpak install -y flathub us.zoom.Zoom com.slack.Slack com.discordapp.Discord
+flatpak install -y flathub us.zoom.Zoom com.slack.Slack com.discordapp.Discord org.mozilla.firefox
 
 echo "Downloading and installing Chrome"
 # Chrome
