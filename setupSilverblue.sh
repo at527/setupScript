d92aee5e00b7b6a18924fd6798e2660cfb2fd4aa
at527 <59618726+at527@users.noreply.github.com>
2021-03-25 23:14:47 -0700

    skip yes no questions

--- setupSilverblue.sh
+++ setupSilverblue.sh
@@ -4,25 +4,25 @@
 rpm-ostree update
 
 # Add gnome-tweaks
-rpm-ostree install gnome-tweaks
+rpm-ostree install gnome-tweaks -y
 
 # Add RPMFusion
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Add codecs
-rpm-ostree groupupdate core
-rpm-ostree groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-rpm-ostree groupupdate sound-and-video
+rpm-ostree groupupdate core -y
+rpm-ostree groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+rpm-ostree groupupdate -y sound-and-video
 
 # Add Flathub
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install Apps
 # Zoom, Slack, Discord
-flatpak install flathub us.zoom.Zoom com.slack.Slack com.discordapp.Discord
+flatpak install -y flathub us.zoom.Zoom com.slack.Slack com.discordapp.Discord
 
 # Chrome
 wget -P ~/Downloads https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-rpm-ostree install ~/Downloads/google-chrome-stable_current_x86_64.rpm
+rpm-ostree install -y ~/Downloads/google-chrome-stable_current_x86_64.rpm
 
 echo Setup Complete. Please Reboot.
