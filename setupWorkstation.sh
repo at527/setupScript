3a509b1a91b754d98d0400d404205f936807405d
at527 <59618726+at527@users.noreply.github.com>
2021-03-26 17:11:09 -0700

    install apps before updating

--- setupWorkstation.sh
+++ setupWorkstation.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
-echo "Disable Suspend"
-sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
-
-echo "Upgrading Packages"
-# Update Packages
-sudo dnf upgrade -y
 
 echo "Installing Gnome Tweaks"
 # Add gnome-tweaks
 sudo dnf install gnome-tweaks -y
 
-echo "Enabling RPMFusion"
-# Add RPMFusion
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-echo "Installing Media Codecs"
-# Add codecs
-sudo dnf groupupdate core -y
-sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf groupupdate -y sound-and-video
+echo "Disable Suspend"
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
 echo "Adding Flathub"
 # Add Flathub
@@ -34,3 +21,19 @@ echo "Downloading and installing Chrome"
 # Chrome
 wget -P ~/Downloads https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 sudo dnf install -y ~/Downloads/google-chrome-stable_current_x86_64.rpm
+
+echo "Upgrading Packages"
+# Update Packages
+sudo dnf upgrade -y
+
+echo "Enabling RPMFusion"
+# Add RPMFusion
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+echo "Installing Media Codecs"
+# Add codecs
+sudo dnf groupupdate core -y
+sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate -y sound-and-video
+
+
